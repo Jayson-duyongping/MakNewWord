517cc833cc5236e7eb33fba4907dbc83ff619110
duyongping <786022775@qq.com>
2019-04-12 17:50:12 +0800

    ...

---
+++ app/src/main/java/com/mak/newword/show/activity/SettingActivity.java
@@ -0,0 +1,23 @@
+package com.mak.newword.show.activity;
+
+import com.mak.newword.R;
+import com.mak.newword.base.BaseFragmentActivity;
+
+public class SettingActivity extends BaseFragmentActivity {
+
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_setting;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
